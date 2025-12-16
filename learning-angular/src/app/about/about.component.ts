c849379c313ddddfca2b6fcece023fbc7900592e
Robertp Scarci <robertoscarci.02@gmail.com>
2025-12-16 14:16:42 +0100

    Display resources on About page using HttpClient

--- learning-angular/src/app/about/about.component.ts
+++ learning-angular/src/app/about/about.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NgFor, AsyncPipe } from '@angular/common';
+import { ResourcesService, Resource } from '../resources.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [NgFor, AsyncPipe],
   template: `
     <main class="page">
       <h1>About this app</h1>
-      <p>This app is for learning Angular step by step: components, services, and routing.</p>
+      <p>This app is for learning Angular step by step.</p>
+
+      <section class="resources">
+        <h2>Useful resources</h2>
+        <ul>
+          <li *ngFor="let r of resources$ | async">
+            <a [href]="r.url" target="_blank">{{ r.title }}</a>
+            <small>({{ r.type }})</small>
+          </li>
+        </ul>
+      </section>
     </main>
   `,
 })
-export class AboutComponent {}
+export class AboutComponent {
+  private resourcesService = inject(ResourcesService);
+  resources$: Observable<Resource[]> = this.resourcesService.getResources();
+}
