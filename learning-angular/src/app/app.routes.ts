b82daed0fbe370be8d0a7b45409df96876548d12
RobertoScarci <robertoscarci.02@gmail.com>
2025-12-18 14:23:18 +0100

    update routes

--- learning-angular/src/app/app.routes.ts
+++ learning-angular/src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { App } from './app';
 import { AboutComponent } from './about/about.component';
 import { TopicDetailComponent } from './topic-detail.component'; 
+import { HomeComponent } from './home/home.component';
+
 
 export const routes: Routes = [
   { path: '', component: App },              // home page
+  { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },  
   { path: 'topics/:title', component: TopicDetailComponent },          // fallback
   { path: '**', redirectTo: '' },  
